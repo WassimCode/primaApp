7b60ce25208a9abf62b0e00d98c528b688280bcd
WassimCode <wassbcr@gmail.com>
2023-10-12 15:09:22 +0200

    test2

--- tesetApp2/tesetApp2/Program.cs
+++ tesetApp2/tesetApp2/Program.cs
@@ -8,7 +8,7 @@ L'app deve modellare un'automibile per farlo deve registrare le informazioni rel
 - colore
 
 
-
+*/
 Auto a1 = new Auto();
 a1.displayData();
 
@@ -16,7 +16,7 @@ Auto a2 = new Auto("Ford", "Mustang", "Red", Auto.motori.benzina);
 a2.displayData();
 
 Console.ReadLine();
-*/
+
 
 class Auto
 {
@@ -35,56 +35,62 @@ class Auto
     public string colore;
     public motori tipoMotore;
 
-    public Auto() {
+
+
+    public Auto()
+    {
         this.marca = "";
-        this.modello= "";
+        this.modello = "";
         this.colore = "";
         this.tipoMotore = motori.indefinito;
     }
 
 
 
-    public Auto(string marca, string modello, string colore,  motori tipoMotore) {
+    public Auto(string marca, string modello, string colore, motori tipoMotore)
+    {
         this.marca = marca;
-        this.modello= modello;
+        this.modello = modello;
         this.colore = colore;
         this.tipoMotore = tipoMotore;
     }
-/*
-    public void displayData()
-    {
-        Console.WriteLine("Marca : " + this.marca);
-        Console.WriteLine("modello : " + this.modello);
-        Console.WriteLine("colore :" + this.colore);
-        Console.WriteLine("tipoMotore: "+  this.tipoMotore);
-        Console.WriteLine("\n-------------------\n");
+    /*
+        public void displayData()
+        {
+            Console.WriteLine("Marca : " + this.marca);
+            Console.WriteLine("modello : " + this.modello);
+            Console.WriteLine("colore :" + this.colore);
+            Console.WriteLine("tipoMotore: "+  this.tipoMotore);
+            Console.WriteLine("\n-------------------\n");
 
-    }
-  */  
-   
+        }
+      */
+
 
     public void displayData()
     {
         Console.WriteLine(generaData());
-            
+
     }
-     
-    public string generaData(){
-        
+
+
+    public string generaData()
+    {
+
         string result = "";
 
         result += "\nMarca : " + this.marca;
-        
+
         result += "\nmodello : " + this.modello;
-        
+
         result += "\ncolore :" + this.colore;
-      
-        result += "\ntipoMotore: "+  this.tipoMotore;
+
+        result += "\ntipoMotore: " + this.tipoMotore;
 
         return result;
-    
+
     }
- 
+
 
 
 }
