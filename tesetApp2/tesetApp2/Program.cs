10e7b719be3386694d3d6a15f8b461d1fce78172
WassimCode <wassbcr@gmail.com>
2023-10-12 17:07:46 +0200

    Added new feauture

--- tesetApp2/tesetApp2/Program.cs
+++ tesetApp2/tesetApp2/Program.cs
@@ -20,26 +20,56 @@ a2.displayData();
 
 Auto a3 = new Auto("Ferrari", "LaFerrari", "Rossa", Auto.motori.diesel, 20);
 
-a3.turnOn();
 char choise = ' ';
 
 while (choise != 'z') {
-    Console.WriteLine("Select the next action:");
+    Console.Clear();
+    Console.WriteLine("Menu for "+ a3.marca + " " +  a3.modello +":\n");
     Console.WriteLine("\n a) Change fuel level.");
     Console.WriteLine("\n b) Turn on.");
     Console.WriteLine("\n c) Turn off.");
+    Console.WriteLine("\n d) Show details.");
+    Console.WriteLine("\n e) Show state.");
     Console.WriteLine("\n z) Exit.");
     choise = Convert.ToChar(Console.ReadLine());
     switch (choise)
     {
         case 'a':
+            Console.Clear();
             a3.changeFuelLevel();
             break;        
         case 'b':
+            Console.Clear();
+
             a3.turnOn();
+            Console.WriteLine("\nPress enter to go back to the menu...");
+            Console.ReadLine();
             break;        
         case 'c':
+            Console.Clear();
+
             a3.turnOff();
+            Console.WriteLine("\nPress enter to go back to the menu...");
+            Console.ReadLine();
+            break;        
+        case 'd':
+            Console.Clear();
+
+            a3.displayData();
+            Console.WriteLine("\nPress enter to go back to the menu...");
+            Console.ReadLine();
+            break;       
+        case 'e':
+            Console.Clear();
+
+            a3.showState();
+            Console.WriteLine("\nPress enter to go back to the menu...");
+            Console.ReadLine();
+            break;
+        case 'z':
+            Console.Clear();
+            Console.WriteLine("\nClosed with success!");
+
             break;
         default:
             break;
@@ -145,8 +175,39 @@ class Auto
 
         result += "\ntipoMotore: " + this.tipoMotore;
 
+        result += "\nFuel: " + this.fuel;
+
+
+        if (this.isOn)
+        {
+            result += "\nTurned on.";
+        }
+        else
+        {
+            result += "\nTurned off";
+        }
+   
         return result;
 
+    }
+
+
+    public void showState()
+    {
+
+        string result = "";
+
+
+        if (this.isOn)
+        {
+            result += "\nTurned on.";
+        }
+        else
+        {
+            result += "\nTurned off";
+        }
+
+        Console.WriteLine(result);
     }
 
     /// <summary>
