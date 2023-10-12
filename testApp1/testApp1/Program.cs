bc4c35f54a4678df5a18b6dda62b768104f3abf8
WassimCode <wassbcr@gmail.com>
2023-10-12 12:57:27 +0200

    created new class 'Auto'

--- testApp1/testApp1/Program.cs
+++ testApp1/testApp1/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*App: Gestione Automobili
+
+L'app deve modellare un'automibile per farlo deve registrare le informazioni relative a:
+- marca
+- modello
+- colore
+*/
+
+
+Auto a1 = new Auto();
+a1.displayData();
+
+Auto a2 = new Auto("Ford", "Mustang", "Red", Auto.motori.benzina);
+a2.displayData();
+
+Console.ReadLine();
+
+
+class Auto{
+    // Tipi personlizzati
+    public enum motori
+    {
+        benzina,
+        diesel,
+        elettrico
+    }
+
+    //  Proprietà
+    public string marca;
+    public string modello;
+    public string colore;
+    public motori tipoMotore;
+
+    public Auto() {
+        this.marca = "";
+        this.modello= "";
+        this.colore = "";
+        this.tipoMotore = motori.benzina;
+    }
+
+    public Auto(string marca, string modello, string colore,  motori tipoMotore) {
+        this.marca = marca;
+        this.modello= modello;
+        this.colore = colore;
+        this.tipoMotore = tipoMotore;
+    }
+
+    public void displayData()
+    {
+        Console.WriteLine("Marca : " + this.marca);
+        Console.WriteLine("modello : " + this.modello);
+        Console.WriteLine("colore :" + this.colore);
+        Console.WriteLine("tipoMotore: "+  this.tipoMotore);
+        Console.WriteLine("\n___________________\n");
+    }
+    
+
+}
