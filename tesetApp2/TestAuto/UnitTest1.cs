b6fe8c3cf0602d81c46f3aea0b9ea2aa7180e054
Ultrazebs <riki_zottoli@hotmail.it>
2023-10-18 11:56:53 +0200

    added test for accelera() method

--- tesetApp2/TestAuto/UnitTest1.cs
+++ tesetApp2/TestAuto/UnitTest1.cs
@@ -25,6 +25,25 @@ namespace TestAuto;
             throw new Exception("");
             }
             
-          } 
-    
-    }
+          }
+
+
+        [TestMethod]
+        public void TestSpeedUp()
+        {
+            // declaring the obj Auto with parameters
+            Auto a2 = new Auto("Ford", "Mustang", "Red", Auto.motori.benzina, 10);
+            a2.chilometraggio = 20;
+        // turning on the car
+            a2.accelera(20);
+
+            // showing the stats to check
+            if (a2.chilometraggio == 40) ;
+            //ok
+            else
+            {
+            throw new Exception("Speed is not synchronized");
+            }
+
+        }
+}
