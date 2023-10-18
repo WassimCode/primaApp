5d1e306cc3ec14c06741e33762b27fe86f3cb652
WassimCode <wassbcr@gmail.com>
2023-10-18 12:06:42 +0200

    FIXED DUPLO

--- tesetApp2/TestAuto/UnitTest1.cs
+++ tesetApp2/TestAuto/UnitTest1.cs
@@ -1,30 +1,4 @@
-using testApp2;
 
-namespace TestAuto;
-
-
-    [TestClass]
-    public class UnitTest1
-    {
-        /// <summary>
-        /// Testa Auto.turnOn
-        /// </summary>
-        [TestMethod]
-        public void TestTurnOn()
-        {
-            // declaring the obj Auto with parameters
-            Auto a2 = new Auto("Ford", "Mustang", "Red", Auto.motori.benzina, 10);
-
-            // turning on the car
-            a2.turnOn();
-
-            // showing the stats to check
-            if (a2.showStateOut() == "\nTurned on.") ;
-            //ok
-            else {
-            throw new Exception("");
-            }
-            
 using testApp2;
 
 namespace TestAuto;
