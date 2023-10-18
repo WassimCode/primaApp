b225527718fdc97979714ed99edf3d56737328c9
WassimCode <wassbcr@gmail.com>
2023-10-18 11:44:41 +0200

    display modified

--- tesetApp2/tesetApp2/Auto.cs
+++ tesetApp2/tesetApp2/Auto.cs
@@ -97,6 +97,8 @@ public class Auto
 
         result += "\nFuel: " + this.fuel;
 
+        result += "\nSpeed : " + this.chilometraggio;
+
         result += "\nSaved in account fuel: " + this.storedInAccountFuel;
 
 
