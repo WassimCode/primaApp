7e9d9acc3f61bea6cdfcb7e352598034837635d9
WassimCode <wassbcr@gmail.com>
2023-10-19 11:44:30 +0200

    temp auto menu

---
+++ tesetApp2/formAuto/Form1.cs
@@ -0,0 +1,19 @@
+namespace formAuto
+{
+    public partial class Form1 : Form
+    {
+        // proprietà utili aggiunte
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ tesetApp2/formAuto/Program.cs
@@ -0,0 +1,19 @@
+namespace formAuto
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            testApp2.AutoInterattiva a3 = new testApp2.AutoInterattiva("Ferrari", "LaFerrari", "Rossa", testApp2.Auto.motori.diesel, 20, 20);
+
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
