37a9a0cac2dd54917e2904eb70277df716b1bf56
WassimCode <wassbcr@gmail.com>
2023-10-19 15:09:52 +0200

    sigma coder

--- tesetApp2/AutoInterattivaForm/Form1.Designer.cs
+++ tesetApp2/AutoInterattivaForm/Form1.Designer.cs
@@ -33,31 +33,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
             button2 = new Button();
-            label3 = new Label();
             button3 = new Button();
             button4 = new Button();
             button5 = new Button();
             button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
             button9 = new Button();
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(47, 57);
-            button1.Name = "button1";
-            button1.Size = new Size(291, 58);
-            button1.TabIndex = 2;
-            button1.Text = "Aggiungi quantità di carburante personalizzata";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(47, 135);
+            button2.Location = new Point(37, 57);
             button2.Name = "button2";
             button2.Size = new Size(291, 58);
             button2.TabIndex = 4;
@@ -65,19 +53,10 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(47, 154);
-            label3.Name = "label3";
-            label3.Size = new Size(127, 20);
-            label3.TabIndex = 3;
-            label3.Text = "Rifornisci del 10%";
-            // 
             // button3
             // 
             button3.BackColor = Color.FromArgb(192, 255, 192);
-            button3.Location = new Point(296, 388);
+            button3.Location = new Point(37, 388);
             button3.Name = "button3";
             button3.Size = new Size(112, 50);
             button3.TabIndex = 5;
@@ -88,7 +67,7 @@
             // button4
             // 
             button4.BackColor = Color.FromArgb(255, 192, 192);
-            button4.Location = new Point(414, 388);
+            button4.Location = new Point(216, 388);
             button4.Name = "button4";
             button4.Size = new Size(112, 50);
             button4.TabIndex = 6;
@@ -98,7 +77,7 @@
             // 
             // button5
             // 
-            button5.Location = new Point(471, 57);
+            button5.Location = new Point(37, 135);
             button5.Name = "button5";
             button5.Size = new Size(291, 58);
             button5.TabIndex = 7;
@@ -108,7 +87,7 @@
             // 
             // button6
             // 
-            button6.Location = new Point(471, 135);
+            button6.Location = new Point(37, 218);
             button6.Name = "button6";
             button6.Size = new Size(291, 58);
             button6.TabIndex = 8;
@@ -116,38 +95,35 @@
             button6.UseVisualStyleBackColor = true;
             button6.Click += button6_Click;
             // 
-            // button7
-            // 
-            button7.BackColor = Color.FromArgb(255, 255, 192);
-            button7.Location = new Point(47, 212);
-            button7.Name = "button7";
-            button7.Size = new Size(291, 59);
-            button7.TabIndex = 9;
-            button7.Text = "Show Speed";
-            button7.UseVisualStyleBackColor = false;
-            button7.Click += button7_Click;
-            // 
-            // button8
-            // 
-            button8.BackColor = Color.FromArgb(255, 255, 192);
-            button8.Location = new Point(471, 212);
-            button8.Name = "button8";
-            button8.Size = new Size(291, 59);
-            button8.TabIndex = 10;
-            button8.Text = "Show State";
-            button8.UseVisualStyleBackColor = false;
-            button8.Click += button8_Click;
-            // 
             // button9
             // 
             button9.BackColor = Color.FromArgb(255, 255, 192);
-            button9.Location = new Point(296, 315);
+            button9.Location = new Point(37, 301);
             button9.Name = "button9";
-            button9.Size = new Size(230, 55);
+            button9.Size = new Size(291, 55);
             button9.TabIndex = 11;
             button9.Text = "Show Details";
             button9.UseVisualStyleBackColor = false;
             button9.Click += button9_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(505, 409);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(283, 29);
+            progressBar1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Cambria Math", 13.8F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.ForeColor = Color.Cyan;
+            label1.Location = new Point(711, 330);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 129);
+            label1.TabIndex = 13;
+            label1.Text = " ";
             // 
             // Form1
             // 
@@ -156,16 +132,14 @@
             BackgroundImage = Properties.Resources.andrew_tate;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(label1);
             Controls.Add(button9);
-            Controls.Add(button8);
-            Controls.Add(button7);
             Controls.Add(button6);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
-            Controls.Add(label3);
-            Controls.Add(button1);
             MinimumSize = new Size(818, 497);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
@@ -175,15 +149,13 @@
         }
 
         #endregion
-        private Button button1;
         private Button button2;
-        private Label label3;
         private Button button3;
         private Button button4;
         private Button button5;
         private Button button6;
-        private Button button7;
-        private Button button8;
         private Button button9;
+        private ProgressBar progressBar1;
+        private Label label1;
     }
 }
