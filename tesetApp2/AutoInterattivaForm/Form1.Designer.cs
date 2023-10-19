9bdcc4a1e15c7ed6269116d9067bdd66c0dbf6a7
WassimCode <wassbcr@gmail.com>
2023-10-19 12:27:31 +0200

    fatto nulla

--- tesetApp2/AutoInterattivaForm/Form1.Designer.cs
+++ tesetApp2/AutoInterattivaForm/Form1.Designer.cs
@@ -158,7 +158,9 @@
             Controls.Add(button2);
             Controls.Add(label3);
             Controls.Add(button1);
+            MinimumSize = new Size(818, 497);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Car Commands Page";
             ResumeLayout(false);
             PerformLayout();
